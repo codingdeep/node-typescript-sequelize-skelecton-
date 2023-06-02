c3013b64fa80a10eb83db31c247647cffa0bf5a7
Rubel Ahmmed <rahmmed@redhat6.8>
2023-06-03 01:50:33 +0600

    initial commit

---
+++ config/db/db.data.js
@@ -0,0 +1,26 @@
+module.exports = {
+    development: {
+        username: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
+        host: "localhost",
+        dialect: "postgres",
+        port: process.env.DB_PORT
+    },
+    test: {
+        username: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
+        host: "localhost",
+        dialect: "postgres",
+        port: process.env.DB_PORT
+    },
+    production: {
+        username: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
+        host: "127.0.0.1",
+        dialect: "postgres",
+        port: process.env.DB_PORT
+    }
+}
---
+++ prettier.config.js
@@ -0,0 +1,11 @@
+const options = {
+    arrowParens: "avoid",
+    tabWidth: 2,
+    printWidth: 100,
+    singleQuote: true,
+    bracketSpacing: true,
+    endOfLine: 'lf',
+    semi: false,
+    trailingComma: false
+}
+export default options;
